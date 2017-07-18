66afad99f78a8962f0bc7a211f2ef5db86bf8dba
Stephen Doherty <stemado@outlook.com>
2017-07-17 21:06:25 -0500

    WhiteNoise added

--- bootcamp/wsgi.py
+++ bootcamp/wsgi.py
@@ -2,16 +2,11 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
-
-application = get_wsgi_application()
 
