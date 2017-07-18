e688480229579e7fc89c376a135d4c0044ecc699
Stephen Doherty <stemado@outlook.com>
2017-07-17 20:27:08 -0500

    added debug

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -11,7 +11,7 @@ PROJECT_DIR = Path(__file__).parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 DATABASES = {
     'default': dj_database_url.config(
