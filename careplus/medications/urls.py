1f684d47d237479cb705a1ba3e883df9b460e429
Stephen Doherty <stemado@outlook.com>
2017-08-02 21:21:29 -0500

    staging

--- careplus/medications/urls.py
+++ careplus/medications/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from careplus.medications import views
 from extra_views import CreateWithInlinesView, UpdateWithInlinesView, InlineFormSetView
 
-
+# app_name = 'medications'
 urlpatterns = [
 
     url(r'^create_medication/(?P<resident_id>[0-9]+)/$', views.createMedication, name='createMedication'),
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^test/$', views.EditMedicationUpdate.as_view(), name='testMedication'),
     url(r'^(?P<id>[0-9]+)/$', views.medication, name='medication'),
     url(r'^testcsv/$', views.csv_view, name='csvView'),
+    # url(r'^testpdf/(?P<mar_id>[0-9]+)/$', views.pdfNewView, name='pdfview'),
     url(r'^testpdf/$', views.pdfNewView, name='pdfview'),
     url(r'^edit_medication/(?P<id>[0-9]+)/$', views.editMedication, name='editMedication'),
     url(r'^status/(?P<medication>[0-9]+)/(?P<rx>[0-9]+)/$', views.acceptRefuse, name='acceptRefuse'),
